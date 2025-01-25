647716895a643853394fdf80e93c0b5544763274
Krisofferson Marini <ksmarini@gmail.com>
2025-01-25 08:59:27 -0500

    add parser session

--- credential-sniffer.py
+++ credential-sniffer.py
@@ -7,3 +7,8 @@ from scapy.all import sniff, TCP, IP, Raw
 
 
 parser = argparse.ArgumentParser(description='Credential sniffer using scapy')
+parser.add_argument('--iface', help='Interface to sniff on', required=True)
+parser.add_argument('--filter', help='BPF filter', required=True)
+parser.add_argument('--keywords', help='File contains list of secret keywords to detect', required=True)
+
+opts = parser.parse_args()
